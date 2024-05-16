ee27840b94247d03fdef42253ee7a5b850cdc962
jamiroZuñiga <jamiro.zuniga@est.fi.uncoma.edu.ar>
2024-05-16 03:03:16 -0300

    implementaciones en generico

--- jerarquicas/dinamicas/ArbolGenerico.java
+++ jerarquicas/dinamicas/ArbolGenerico.java
@@ -21,29 +21,61 @@ public class ArbolGenerico{
             }
             return inserto;
        }
-       public Boolean pertenece(Object elem){
+       public Boolean pertenece(Object elem){//retorna true si el elemento pertenece al arbol
             Boolean pertenece=false;
+            if(!this.esVacia()){
+                NodoGen n=obtenerNodo(this.raiz,elem);//si el elemento esta en el arbol retorna el nodo
+                if(n!=null){
+                    pertenece=true;
+                }
+            }
             return pertenece;
        }
        public NodoGen obtenerNodo(NodoGen n,Object elem){//retorna el nodo buscado
             NodoGen ret=null;
-            if(n!=null){
-                if(n.getElem().equals(elem)){
-                    ret=n;
-                }else{
-                    ret=obtenerNodo(n.getHijoIzq(), elem);//si no encuentra por el hijo izquierdo
-                    if(ret!=null){
-                        ret=obtenerNodo(n.getHermanoDer(), elem);//busca por el hijo derecho
-                    }
+            if (n != null){
+                if (n.getElem() == elem){
+                    ret = n;
+                } else {
+                    NodoGen hijo = n.getHijoIzq(); //guardo el primer hijo
+                    boolean flag = false;
+    
+                    while (!flag && hijo != null){ //si no ha sido encontrado y no se terminaron sus hijos
+                        ret = obtenerNodo(hijo, elem);
+                        if (ret != null){ //Nodo encontrado, salgo de la repetitiva
+                            flag = true;
+                        } else {
+                            hijo = hijo.getHermanoDer(); //me muevo a su hermano
+                        }
+                    } 
+                    
                 }
             }
             return ret;
        }
-       public Lista ancestros(){
-
+       public Lista ancestros(Object elem){
+            Lista listAncestros=new Lista();
+            if(!this.esVacia() && obtenerNodo(this.raiz,elem)!=null){// si no esta vacio el arbol y el elemento existe en este 
+                ancestrosRec(listAncestros,this.raiz,elem,false);
+            }
+            return listAncestros;
+       }
+       private void ancestrosRec(Lista list,NodoGen n,Object elem,Boolean v){
+            if(n!=null){
+                 if(n.getElem().equals(elem)){//caso base
+                      list.insertar(elem, list.longitud()+1);
+                      v=true;
+                 }
+                 ancestrosRec(list, n.getHijoIzq(), elem,v);
+                 if(v){
+                    list.insertar(n.getElem(),list.longitud()+1);
+                 }
+                 ancestrosRec(list, n.getHermanoDer(),elem,v);
+            }
        }
        public int altura(){//
-
+             int alt=-1;
+             return alt;
        }
        public void vaciar(){//VACIA EL ARBOL
            this.raiz=null;
@@ -89,5 +121,67 @@ public class ArbolGenerico{
             }
         }
         return ret;
+    }
+    public Lista listarInorden(){
+        Lista salida=new Lista();
+        listarInordenRec(this.raiz,salida);
+        return salida;
+    }
+    private void listarInordenRec(NodoGen n,Lista list){
+        if(n!=null){
+            if(n.getHijoIzq()!=null){//llamado recursivo por el primer hijo de n
+                  listarInordenRec(n.getHijoIzq(),list);
+            }
+            //visita el nodo n
+            list.insertar(n.getElem(),list.longitud()+1);
+            if(n.getHijoIzq()!=null){//llamado recursivo con los otros hijos de n
+               NodoGen hijo=n.getHijoIzq().getHermanoDer();//me muevo a los hermanos del hijo izq
+               while(hijo!=null){
+                   listarInordenRec(hijo,list);
+                   hijo=hijo.getHermanoDer();
+               }
+            }
+        }
+    }
+    public Lista listarPreorden(){
+        Lista list=new Lista();
+        if(!this.esVacia()){
+             preOrdenRec(this.raiz,list);
+        }
+        return list;
+    }
+    private void preOrdenRec(NodoGen n ,Lista list){
+        if(n!=null){
+            list.insertar(n.getElem(),list.longitud()+1);//visita el nodo n
+            if(n.getHijoIzq()!=null){//llamado recursivo por el primer hijo de n
+                listarInordenRec(n.getHijoIzq(),list);
+            }
+            NodoGen hijo=n.getHijoIzq().getHermanoDer();//me muevo a los hermanos del hijo izq
+            while(hijo!=null){
+               listarInordenRec(hijo,list);
+               hijo=hijo.getHermanoDer();
+            }
+      } 
+    }
+    public Lista listarPosOrden(){
+        Lista list=new Lista();
+        if(!this.esVacia()){
+             posOrdenRec(this.raiz,list);
+        }
+        return list;
+    }
+    private void posOrdenRec(NodoGen n ,Lista list){
+        if(n!=null){
+           
+            if(n.getHijoIzq()!=null){//llamado recursivo por el primer hijo de n
+                listarInordenRec(n.getHijoIzq(),list);
+            }
+            NodoGen hijo=n.getHijoIzq().getHermanoDer();//me muevo a los hermanos del hijo izq
+            while(hijo!=null){
+               listarInordenRec(hijo,list);
+               hijo=hijo.getHermanoDer();
+            } 
+            list.insertar(n.getElem(),list.longitud()+1);//visita el nodo n
+      } 
     }
 }
--- testJerarquicos/TestingArbolGen.java
+++ testJerarquicos/TestingArbolGen.java
@@ -9,13 +9,22 @@ public class TestingArbolGen {
         //cargo arbol
         cargarArbol(arbol);
         System.out.println(arbol.toString());
+        if(arbol.pertenece('H')){
+            System.out.println("el objeto 'H' pertence al arbol");
+        }else{
+            System.out.println("el objeto 'H' NO pertence al arbol");
+        }
+        System.out.println("ancestros de 'D': "+arbol.ancestros('D').toString());
+        System.out.println("Inorden: "+arbol.listarInorden().toString());//muestra el recorrido en inOrden
+        System.out.println("Preorden: "+arbol.listarPreorden().toString());//muestra el recorrido en preOrden
+        System.out.println("Posorden: "+arbol.listarPosOrden().toString());//muestra el recorrido en posOrden
     }
     public static void cargarArbol(ArbolGenerico arbol){
         arbol.insertar('A',null);
         arbol.insertar('B','A');
         arbol.insertar('C','A');
         arbol.insertar('D','A');
-        arbol.insertar('F', 'D');
+        arbol.insertar('F', 'B');
         arbol.insertar('H', 'D');
     }
 }
